e14edf1df87f1f234f15dc02223d2a461495b37f
Martín Romera Sobrado <mromera99@gmail.com>
2020-06-29 22:44:30 +0200

    Added templates and static files directories

--- LandingPage/LandingPage/settings.py
+++ LandingPage/LandingPage/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'LandingPage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates/")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, "static/")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
