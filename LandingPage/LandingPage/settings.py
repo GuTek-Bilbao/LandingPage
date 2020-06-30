ff90ef304ab8dcb18a73ef2e21684cf677e5ee41
Martín Romera Sobrado <mromera99@gmail.com>
2020-06-30 13:33:04 +0200

    Changed and hide secret key

--- LandingPage/LandingPage/settings.py
+++ LandingPage/LandingPage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from . import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fnl(#%&6_&bsd^8vlbs$3_r!jt8=6$=c$o&%ag8gq7w(x2-8e0'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
