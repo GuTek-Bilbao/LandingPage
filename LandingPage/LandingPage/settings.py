6d2c2325d77175ef788d0c5573d07ea3a81f1760
Martín Romera Sobrado <mromera99@gmail.com>
2020-07-04 15:01:58 +0200

    Added media root

--- LandingPage/LandingPage/settings.py
+++ LandingPage/LandingPage/settings.py
@@ -121,5 +121,7 @@ USE_TZ = True
 STATIC_URL = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static/")
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
