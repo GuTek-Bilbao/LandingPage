2cfe28c1515da27bb23c481a1402c21db7bf0aff
Martín Romera Sobrado <mromera99@gmail.com>
2020-07-05 19:29:20 +0200

    Added post url

--- LandingPage/app/urls.py
+++ LandingPage/app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('blog/', views.blog, name="blog")
+    path('blog/', views.blog, name="blog"),
+    path('blog/<slug:slug>', views.post_detail, name="post"),
 ]
