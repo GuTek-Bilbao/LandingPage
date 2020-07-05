8e17ca601dd2f09b9b111c1bb5f3b8acde301a8f
Martín Romera Sobrado <mromera99@gmail.com>
2020-07-05 19:33:33 +0200

    fixed post_view view

--- LandingPage/app/urls.py
+++ LandingPage/app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('blog/', views.blog, name="blog"),
-    path('blog/<slug:slug>', views.post_detail, name="post"),
+    path('blog/<slug:slug>', views.post_view, name="post"),
 ]
