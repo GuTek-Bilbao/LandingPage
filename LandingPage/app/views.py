fa201dbced33891dfbbbc498c77d1dc7a12b765c
Martín Romera Sobrado <mromera99@gmail.com>
2020-07-07 19:21:10 +0200

    Changed the query for the posts

--- LandingPage/app/views.py
+++ LandingPage/app/views.py
@@ -9,9 +9,8 @@ def index(request):
 
 
 def blog(request):
-    posts = models.Post.objects.order_by('created_on')
-    contents = models.PostContent.objects.filter(lang=request.LANGUAGE_CODE)
-    return render(request, "blog.html", {'contents' : contents, 'posts':posts})
+    posts = models.PostContent.objects.filter(lang=request.LANGUAGE_CODE, post__status=1)
+    return render(request, "blog.html", {'posts':posts})
 
 def post_view(request, slug):
     content = models.PostContent.objects.filter(post__slug=slug, lang=request.LANGUAGE_CODE)
